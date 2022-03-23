db8b1098e27016848e4ec23121280776fde350f3
XiaoPan <2489964425@qq.com>
2022-03-23 21:42:53 +0800

    beforeNode-将一个节点添加到另一个节点的前面

--- src/pDom.js
+++ src/pDom.js
@@ -10,6 +10,9 @@ window.pDom = {
         // 没有插入后面的方法，所以插入到 node 后面节点的前面
         node.parentNode.insertBefore(newNode, node.nextSibling);
     },
-
+    // 在 node 的前面插入 newNode
+    beforeNode(node, newNode){
+        node.parentNode.insertBefore(node, newNode);
+    },
 
 }

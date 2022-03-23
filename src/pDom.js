5840bdbdf27b07a3087d46d70162f295234ab0b1
XiaoPan <2489964425@qq.com>
2022-03-23 21:42:42 +0800

    afterNode-将一个节点添加到另一个节点的后面

--- src/pDom.js
+++ src/pDom.js
@@ -5,6 +5,11 @@ window.pDom = {
         template.innerHTML = string.trim();
         return template.content.firstChild;
     },
+    // 在 node 的后面插入 newNode
+    afterNode(node, newNode){
+        // 没有插入后面的方法，所以插入到 node 后面节点的前面
+        node.parentNode.insertBefore(newNode, node.nextSibling);
+    },
 
 
 }
